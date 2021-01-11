f2e9ab75103f78cccd9db6d4f787fdd2ed474f90
WiseM [PC Perso] <samuel.litzler39@gmail.com>
2021-01-11 17:44:25 +0100

    début d'un navbar

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './js/App';
-import reportWebVitals from './js/reportWebVitals';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('todoasso')
-);
+ReactDOM.render(<App />,document.getElementById('todoasso'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
